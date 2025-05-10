a7260ca33d6ab6379e90428e18958eebf3bbc696
Diwas Rai <diwasrai38@yahoo.com>
2025-05-10 23:46:34 +0100

    update middleware again to maybe fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(["/protected(.*)"]);
-const isWebhookRoute = createRouteMatcher(["/api/users/webhook"])
 
 export default clerkMiddleware(async (auth, req) => {
-    if (isWebhookRoute(req)) return;
     if (isProtectedRoute(req)) await auth.protect();
 });
 
@@ -13,6 +11,6 @@ export const config = {
         // Skip Next.js internals and all static files, unless found in search params
         "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
         // Always run for API routes
-        "/(api|trpc)(.*)",
+        "/(trpc)(.*)",
     ],
 };
