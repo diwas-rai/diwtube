bf92fa3f2850d756227f7eaaa05be08e4bee80f1
Diwas Rai <diwasrai38@yahoo.com>
2025-05-12 20:51:28 +0100

    create seeding script

---
+++ src/scripts/seed-categories.ts
@@ -0,0 +1,40 @@
+import { db } from "@/db";
+import { categories } from "@/db/schema";
+
+const categoryNames = [
+    "Cars and vehicles",
+    "Comedy",
+    "Education",
+    "Entertainment",
+    "Film and animation",
+    "Gaming",
+    "How-to and style",
+    "Music",
+    "News and politics",
+    "Non-profits and activism",
+    "People and blogs",
+    "Pets and animals",
+    "Science and technology",
+    "Sport",
+    "Travel and events",
+];
+
+async function main() {
+    console.log("Seeding categories...");
+
+    try {
+        const values = categoryNames.map((name) => ({
+            name,
+            description: `Videos related to ${name.toLowerCase()}`,
+        }));
+
+        await db.insert(categories).values(values)
+
+        console.log("Categories seeded succesfully!")
+    } catch (error) {
+        console.error("Error seeding categories: ", error);
+        process.exit(1);
+    }
+}
+
+main();
