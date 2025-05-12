9497ee83757c96cd4781a1d823a942d726d75453
Diwas Rai <diwasrai38@yahoo.com>
2025-05-12 20:49:20 +0100

    create categories schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -14,3 +14,15 @@ export const users = pgTable(
     // index clerkId to query faster
     (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]
 );
+
+export const categories = pgTable(
+    "categories",
+    {
+        id: uuid("id").primaryKey().defaultRandom(),
+        name: text("name").notNull().unique(),
+        description: text("description"),
+        createdAt: timestamp("created_at").defaultNow().notNull(),
+        updatedAt: timestamp("updated_at").defaultNow().notNull(),
+    },
+    (t) => [uniqueIndex("name_idx").on(t.name)]
+);
