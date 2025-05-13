0bf7fd82552580369868f0bb132df006f5d1dbf8
Diwas Rai <diwasrai38@yahoo.com>
2025-05-13 21:27:38 +0100

    add basic functionality to studio

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -1,12 +1,20 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { PlusIcon } from "lucide-react";
+import { trpc } from "@/trpc/client";
+import { Loader2Icon, PlusIcon } from "lucide-react";
 
 export const StudioUploadModal = () => {
-    // TODO: Add functionality
+    const utils = trpc.useUtils();
+    const create = trpc.videos.create.useMutation({
+        // When succesfully created a video, invalidate the current state and cause a reload
+        onSuccess: () => {
+            utils.studio.getMany.invalidate();
+        },
+    });
+
     return (
-        <Button variant="secondary">
-            <PlusIcon />
+        <Button variant="secondary" onClick={() => create.mutate()} disabled={create.isPending}>
+            {create.isPending ? <Loader2Icon className="animate-spin" /> : <PlusIcon />}
             Create
         </Button>
     );
---
+++ src/modules/studio/ui/view/studio-view.tsx
@@ -0,0 +1,9 @@
+import { VideosSections } from "../sections/videos-sections"
+
+export const StudioView = () => {
+    return (
+        <div>
+            <VideosSections />
+        </div>
+    )
+}
