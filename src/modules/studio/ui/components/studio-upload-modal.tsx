4b89ec77dd9071ef500478a3ec536585746b30a3
Diwas Rai <diwasrai38@yahoo.com>
2025-05-13 21:34:26 +0100

    add toast for notifs

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -2,13 +2,18 @@
 import { Button } from "@/components/ui/button";
 import { trpc } from "@/trpc/client";
 import { Loader2Icon, PlusIcon } from "lucide-react";
+import { toast } from "sonner";
 
 export const StudioUploadModal = () => {
     const utils = trpc.useUtils();
     const create = trpc.videos.create.useMutation({
-        // When succesfully created a video, invalidate the current state and cause a reload
+        // When successfully created a video, invalidate the current state and cause a reload
         onSuccess: () => {
+            toast.success("Video uploaded successfully!");
             utils.studio.getMany.invalidate();
+        },
+        onError: () => {
+            toast.error("Something went wrong... 🫤");
         },
     });
 
