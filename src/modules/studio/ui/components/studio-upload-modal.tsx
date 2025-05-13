bcecef68952d873838aeeab9d05e46278213453a
Diwas Rai <diwasrai38@yahoo.com>
2025-05-13 20:04:58 +0100

    add button to upload

---
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
+
+export const StudioUploadModal = () => {
+    // TODO: Add functionality
+    return (
+        <Button variant="secondary">
+            <PlusIcon />
+            Create
+        </Button>
+    );
+};
