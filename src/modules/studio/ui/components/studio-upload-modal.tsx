4af66bec04a82d9659e1313f00e476dbffc3c280
Diwas Rai <diwasrai38@yahoo.com>
2025-05-14 01:46:12 +0100

    add functioanlity for uploading videos

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -1,8 +1,10 @@
 "use client";
+import { ResponsiveModal } from "@/components/responsive-dialog";
 import { Button } from "@/components/ui/button";
 import { trpc } from "@/trpc/client";
 import { Loader2Icon, PlusIcon } from "lucide-react";
 import { toast } from "sonner";
+import { StudioUploader } from "./studio-uploader";
 
 export const StudioUploadModal = () => {
     const utils = trpc.useUtils();
@@ -18,9 +20,23 @@ export const StudioUploadModal = () => {
     });
 
     return (
-        <Button variant="secondary" onClick={() => create.mutate()} disabled={create.isPending}>
-            {create.isPending ? <Loader2Icon className="animate-spin" /> : <PlusIcon />}
-            Create
-        </Button>
+        <>
+            <ResponsiveModal
+                title="Upload a video"
+                open={!!create.data?.url}
+                onOpenChange={() => create.reset()}
+            >
+                <p>This will be an uploader</p>
+                {create.data?.url ? (
+                    <StudioUploader endpoint={create.data.url} />
+                ) : (
+                    <Loader2Icon />
+                )}
+            </ResponsiveModal>
+            <Button variant="secondary" onClick={() => create.mutate()} disabled={create.isPending}>
+                {create.isPending ? <Loader2Icon className="animate-spin" /> : <PlusIcon />}
+                Create
+            </Button>
+        </>
     );
 };
