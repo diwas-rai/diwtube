e2add2841d4b4a1789ef8f3cc9e88e91e9e771c6
Diwas Rai <diwasrai38@yahoo.com>
2025-05-18 18:41:56 +0100

    get openai integration working

--- src/app/api/videos/workflows/title/route.ts
+++ src/app/api/videos/workflows/title/route.ts
@@ -12,10 +12,45 @@ export const { POST } = serve(async (context) => {
     const input = context.requestPayload as inputType;
     const { videoId, userId } = input;
 
+    const video = await context.run("get-video", async () => {
+        const [existingVideo] = await db
+            .select()
+            .from(videos)
+            .where(and(eq(videos.id, videoId), eq(videos.userId, userId)));
+
+        if (!existingVideo) {
+            throw new Error("Not found");
+        }
+
+        return existingVideo;
+    });
+
+    const { body } = await context.api.openai.call("generate-title", {
+        token: process.env.OPENAI_API_KEY!,
+        operation: "chat.completions.create",
+        body: {
+            model: "gpt-4o",
+            messages: [
+                {
+                    role: "system",
+                    content: process.env.GENERATE_TITLE_PROMPT,
+                },
+                {
+                    role: "user",
+                    content:
+                        "This is a youtube video testing if this openAI integration is going to work with upstash workflows!",
+                },
+            ],
+        },
+    });
+
+    // extract the title and remove any quotes
+    const generatedTitle = body.choices[0].message.content?.replaceAll('"', "");
+
     await context.run("update-video", async () => {
         await db
             .update(videos)
-            .set({ title: "Updated from background job" })
-            .where(and(eq(videos.id, videoId), eq(videos.userId, userId)));
+            .set({ title: generatedTitle ?? video.title })
+            .where(and(eq(videos.id, video.id), eq(videos.userId, video.userId)));
     });
 });
