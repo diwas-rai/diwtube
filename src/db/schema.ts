a39d67a66f8cd14711b3331fbcced968ca8d38f0
Diwas Rai <diwasrai38@yahoo.com>
2025-05-31 14:49:04 +0200

    add schema and procedures for subscriptions

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -29,7 +29,13 @@ export const users = pgTable(
 export const userRelations = relations(users, ({ many }) => ({
     videos: many(videos), // user can have many videos
     videoViews: many(videoViews), // user can have many video views
-    videoReactions: many(videoReactions) // user can have many video reactions
+    videoReactions: many(videoReactions), // user can have many video reactions
+    subscriptions: many(subscriptions, {
+        relationName: "subscription_viewer_id_fkey",
+    }),
+    subscribers: many(subscriptions, {
+        relationName: "subscription_creator_id_fkey",
+    }),
 }));
 
 export const categories = pgTable(
@@ -94,7 +100,7 @@ export const videoRelations = relations(videos, ({ one, many }) => ({
     // videos can have many views
     videoViews: many(videoViews),
     // video can have many reactions
-    videoReactions: many(videoReactions)
+    videoReactions: many(videoReactions),
 }));
 
 export const videoViews = pgTable(
@@ -164,3 +170,36 @@ export const videoReactionRelations = relations(videoReactions, ({ one }) => ({
 export const videoReactionSelectSchema = createSelectSchema(videoReactions);
 export const videoReactionInsertSchema = createInsertSchema(videoReactions);
 export const videoReactionUpdateSchema = createUpdateSchema(videoReactions);
+
+export const subscriptions = pgTable(
+    "subscriptions",
+    {
+        viewerId: uuid("user_id")
+            .references(() => users.id, { onDelete: "cascade" })
+            .notNull(),
+        creatorId: uuid("creator_id")
+            .references(() => users.id, { onDelete: "cascade" })
+            .notNull(),
+        createdAt: timestamp("created_at").defaultNow().notNull(),
+        updatedAt: timestamp("updated_at").defaultNow().notNull(),
+    },
+    (t) => [
+        primaryKey({
+            name: "susbcriptions_pk",
+            columns: [t.viewerId, t.creatorId],
+        }),
+    ]
+);
+
+export const subscriptionRelations = relations(subscriptions, ({ one }) => ({
+    viewerId: one(users, {
+        fields: [subscriptions.viewerId],
+        references: [users.id],
+        relationName: "subscription_viewer_id_fkey",
+    }),
+    creatorId: one(users, {
+        fields: [subscriptions.creatorId],
+        references: [users.id],
+        relationName: "subscription_creator_id_fkey",
+    }),
+}));
