fa5572fd3c79f17a45eeefd41f52f22cb5de27c1
Diwas Rai <diwasrai38@yahoo.com>
2025-06-01 18:32:55 +0200

    add dynamic comment count

--- src/modules/videos/ui/sections/comments-section.tsx
+++ src/modules/videos/ui/sections/comments-section.tsx
@@ -31,8 +31,7 @@ const CommentsSectionSuspense = ({ videoId }: CommentsSectionProps) => {
     return (
         <div className="mt-6">
             <div className="flex flex-col gap-6">
-                {/* update this to use correct comment count */}
-                <h1>0 comments</h1>
+                <h1>{comments.length} comments</h1>
                 <CommentForm videoId={videoId} />
                 <div className="mt-2 flex flex-col gap-4">
                     {comments.map((comment) => (
