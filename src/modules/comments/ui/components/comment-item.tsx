5db97e121f1c19861aa3a1f74b4df76b56ccbe62
Diwas Rai <diwasrai38@yahoo.com>
2025-06-06 23:08:25 +0100

    fix commentItemProps

--- src/modules/comments/ui/components/comment-item.tsx
+++ src/modules/comments/ui/components/comment-item.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { CommentsGetManyOutput } from "../../types";
 
 interface CommentItemProps {
-    comment: CommentsGetManyOutput[number];
+    comment: CommentsGetManyOutput["items"][number];
 }
 
 export const CommentItem = ({ comment }: CommentItemProps) => {
