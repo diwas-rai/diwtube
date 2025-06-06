8839efb305e789fd87739f1de723b95c6e88917c
Diwas Rai <diwasrai38@yahoo.com>
2025-06-06 23:25:11 +0100

    create remove comment procedure

--- src/modules/comments/server/procedures.ts
+++ src/modules/comments/server/procedures.ts
@@ -1,10 +1,32 @@
 import { db } from "@/db";
 import { comments, users } from "@/db/schema";
 import { baseProcedure, createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { TRPCError } from "@trpc/server";
 import { and, count, desc, eq, getTableColumns, lt, or } from "drizzle-orm";
 import { z } from "zod";
 
 export const commentsRouter = createTRPCRouter({
+    remove: protectedProcedure
+        .input(
+            z.object({
+                id: z.string().uuid(),
+            })
+        )
+        .mutation(async ({ input, ctx }) => {
+            const { id: userId } = ctx.user;
+            const { id } = input;
+
+            const [deletedComment] = await db
+                .delete(comments)
+                .where(and(eq(comments.id, id), eq(comments.userId, userId)))
+                .returning();
+
+            if (!deletedComment) {
+                throw new TRPCError({ code: "NOT_FOUND" });
+            }
+
+            return deletedComment;
+        }),
     create: protectedProcedure
         .input(
             z.object({
