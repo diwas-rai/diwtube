c76bcc9c2c469b86e001b625efdd404d12474ee5
Diwas Rai <diwasrai38@yahoo.com>
2025-06-22 14:40:53 +0100

    use mobile components to display video suggestions

--- src/modules/videos/ui/sections/suggestions-section.tsx
+++ src/modules/videos/ui/sections/suggestions-section.tsx
@@ -1,30 +1,52 @@
 "use client";
 
+import { InfiniteScroll } from "@/components/infinite-scroll";
 import { DEFAULT_LIMIT } from "@/constants";
 import { trpc } from "@/trpc/client";
+import { VideoGridCard } from "../components/video-grid-card";
 import { VideoRowCard } from "../components/video-row-card";
 
 interface SuggestionsSectionProp {
   videoId: string;
+  isManual?: boolean;
 }
-export const SuggestionsSection = ({ videoId }: SuggestionsSectionProp) => {
-  const [suggestions] = trpc.suggestions.getMany.useSuspenseInfiniteQuery(
-    {
-      videoId,
-      limit: DEFAULT_LIMIT,
-    },
-    {
-      getNextPageParam: (lastPage) => lastPage.nextCursor,
-    }
-  );
+export const SuggestionsSection = ({
+  videoId,
+  isManual,
+}: SuggestionsSectionProp) => {
+  const [suggestions, query] =
+    trpc.suggestions.getMany.useSuspenseInfiniteQuery(
+      {
+        videoId,
+        limit: DEFAULT_LIMIT,
+      },
+      {
+        getNextPageParam: (lastPage) => lastPage.nextCursor,
+      }
+    );
 
   return (
-    <div>
-      {suggestions.pages.flatMap((page) =>
-        page.items.map((video) => (
-          <VideoRowCard key={video.id} data={video} size="compact" />
-        ))
-      )}
-    </div>
+    <>
+      <div className="hidden space-y-3 md:block">
+        {suggestions.pages.flatMap((page) =>
+          page.items.map((video) => (
+            <VideoRowCard key={video.id} data={video} size="compact" />
+          ))
+        )}
+      </div>
+      <div className="block space-y-10 md:hidden">
+        {suggestions.pages.flatMap((page) =>
+          page.items.map((video) => (
+            <VideoGridCard key={video.id} data={video} onRemove={() => {}} />
+          ))
+        )}
+      </div>
+      <InfiniteScroll
+        isManual={isManual}
+        hasNextPage={query.hasNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+        fetchNextPage={query.fetchNextPage}
+      />
+    </>
   );
 };
