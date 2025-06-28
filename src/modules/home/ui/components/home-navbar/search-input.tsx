4484cf00623654b4c43a75c6513f86eb4e91ab0c
Diwas Rai <diwasrai38@yahoo.com>
2025-06-28 22:03:37 +0100

    implement search functionality

--- src/modules/home/ui/components/home-navbar/search-input.tsx
+++ src/modules/home/ui/components/home-navbar/search-input.tsx
@@ -1,19 +1,55 @@
-import { SearchIcon } from "lucide-react";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { BASE_URL } from "@/constants";
+import { SearchIcon, XIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 export const SearchInput = () => {
-  // TODO: Add search functionality
+  const router = useRouter();
+  const [value, setValue] = useState("");
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const url = new URL("/search", BASE_URL);
+    const newQuery = value.trim();
+
+    url.searchParams.set("query", encodeURIComponent(newQuery));
+
+    if (newQuery === "") {
+      url.searchParams.delete("query");
+    }
+
+    setValue(newQuery);
+    router.push(url.toString());
+  };
 
   return (
-    <form className="flex w-full max-w-[600px]">
+    <form className="flex w-full max-w-[600px]" onSubmit={handleSearch}>
       <div className="relative w-full">
         <input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
           type="text"
           placeholder="Search"
           className="w-full rounded-l-full border py-2 pl-4 pr-12 focus:border-blue-500 focus:outline-none"
         />
-        {/* TODO: add remove search button */}
+        {value && (
+          <Button
+            type="button"
+            variant="ghost"
+            size="icon"
+            onClick={() => setValue("")}
+            className="absolute right-2 top-1/2 -translate-y-1/2 rounded-full"
+          >
+            <XIcon className="text-gray-500" />
+          </Button>
+        )}
       </div>
       <button
+        disabled={!value.trim()}
         type="submit"
         className="dsiabled:opacity-50 rounded-r-full border border-l-0 bg-gray-100 px-5 py-2.5 hover:bg-gray-200 disabled:cursor-not-allowed"
       >
