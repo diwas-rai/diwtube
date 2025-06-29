b8f9aa942855c03a4fb84ed4b261574e08173a2b
Diwas Rai <diwasrai38@yahoo.com>
2025-06-29 17:43:10 +0100

    add getManySubscribed procedure

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -29,6 +29,78 @@ import { UTApi } from "uploadthing/server";
 import { z } from "zod";
 
 export const videosRouter = createTRPCRouter({
+  getManySubscribed: protectedProcedure
+    .input(
+      z.object({
+        cursor: z
+          .object({
+            id: z.string().uuid(),
+            updatedAt: z.date(),
+          })
+          .nullish(),
+        limit: z.number().min(1).max(100),
+      })
+    )
+    .query(async ({ input, ctx }) => {
+      const { cursor, limit } = input;
+      const { id: userId } = ctx.user;
+
+      const viewerSubscriptions = db
+        .$with("viewer_subscriptions")
+        .as(
+          db
+            .select({ userId: subscriptions.creatorId })
+            .from(subscriptions)
+            .where(eq(subscriptions.viewerId, userId))
+        );
+
+      const data = await db
+        .with(viewerSubscriptions)
+        .select({
+          ...getTableColumns(videos),
+          user: users,
+          viewCount: db.$count(videoViews, eq(videoViews.videoId, videos.id)),
+        })
+        .from(videos)
+        .innerJoin(users, eq(videos.userId, users.id))
+        .innerJoin(
+          viewerSubscriptions,
+          eq(viewerSubscriptions.userId, users.id)
+        )
+        .where(
+          and(
+            eq(videos.visibility, "public"),
+            cursor
+              ? or(
+                  lt(videos.updatedAt, cursor.updatedAt),
+                  and(
+                    eq(videos.updatedAt, cursor?.updatedAt),
+                    lt(videos.id, cursor.id)
+                  )
+                )
+              : undefined
+          )
+        )
+        .orderBy(desc(videos.updatedAt), desc(videos.id))
+        // Add 1 to limit to check if more data to load
+        .limit(limit + 1);
+
+      const hasMore = data.length > limit;
+
+      // Remove last item if more data than requested
+      const items = hasMore ? data.slice(0, -1) : data;
+
+      // Set next cursor to last item if there is more data
+      const lastItem = items[items.length - 1];
+      const nextCursor = hasMore
+        ? {
+            id: lastItem.id,
+            updatedAt: lastItem.updatedAt,
+          }
+        : null;
+
+      return { items, nextCursor };
+    }),
   getManyTrending: baseProcedure
     .input(
       z.object({
