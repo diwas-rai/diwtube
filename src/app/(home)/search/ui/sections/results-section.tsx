99b0e4037129ef4eb74c715e93a6c46bba7c2d56
Diwas Rai <diwasrai38@yahoo.com>
2025-07-03 01:48:09 +0100

    refactor ResultsSectionSuspense to remove mobile check and simplify rendering logic

--- src/app/(home)/search/ui/sections/results-section.tsx
+++ src/app/(home)/search/ui/sections/results-section.tsx
@@ -2,7 +2,6 @@
 
 import { InfiniteScroll } from "@/components/infinite-scroll";
 import { DEFAULT_LIMIT } from "@/constants";
-import { useIsMobile } from "@/hooks/use-mobile";
 import { VideoGridCard } from "@/modules/videos/ui/components/video-grid-card";
 import { VideoRowCard } from "@/modules/videos/ui/components/video-row-card";
 import { trpc } from "@/trpc/client";
@@ -28,7 +27,6 @@ export const ResultsSectionSuspense = ({
   query,
   categoryId,
 }: ResultsSectionProps) => {
-  const isMobile = useIsMobile();
   const [results, { fetchNextPage, hasNextPage, isFetchingNextPage }] =
     trpc.search.getMany.useSuspenseInfiniteQuery(
       {
@@ -43,23 +41,20 @@ export const ResultsSectionSuspense = ({
 
   return (
     <>
-      {isMobile ? (
-        <div className="flex flex-col gap-4 gap-y-10">
-          {results.pages.flatMap((page) =>
-            page.items.map((video) => (
-              <VideoGridCard key={video.id} data={video} />
-            ))
-          )}
-        </div>
-      ) : (
-        <div className="flex flex-col gap-4">
-          {results.pages.flatMap((page) =>
-            page.items.map((video) => (
-              <VideoRowCard key={video.id} data={video} />
-            ))
-          )}
-        </div>
-      )}
+      <div className="flex flex-col gap-4 gap-y-10 md:hidden">
+        {results.pages.flatMap((page) =>
+          page.items.map((video) => (
+            <VideoGridCard key={video.id} data={video} />
+          ))
+        )}
+      </div>
+      <div className="hidden flex-col gap-4 md:flex">
+        {results.pages.flatMap((page) =>
+          page.items.map((video) => (
+            <VideoRowCard key={video.id} data={video} />
+          ))
+        )}
+      </div>
       <InfiniteScroll
         hasNextPage={hasNextPage}
         isFetchingNextPage={isFetchingNextPage}
