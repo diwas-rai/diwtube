aa3eb84cd093296f1c8f784bba60bc967e7c4cd2
Diwas Rai <diwasrai38@yahoo.com>
2025-07-03 01:26:31 +0100

    update VideoGetManyOutput type

--- src/modules/videos/types.ts
+++ src/modules/videos/types.ts
@@ -4,5 +4,4 @@ import { inferRouterOutputs } from "@trpc/server";
 export type VideoGetOneOutput =
   inferRouterOutputs<AppRouter>["videos"]["getOne"];
 
-// TODO: change to videosGetMany
-export type VideoGetManyOutput = inferRouterOutputs<AppRouter>["suggestions"]["getMany"]
+export type VideoGetManyOutput = inferRouterOutputs<AppRouter>["videos"]["getMany"]
