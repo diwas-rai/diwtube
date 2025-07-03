776a0abc112f929e59d4fc8709ab4ff736cb9032
Diwas Rai <diwasrai38@yahoo.com>
2025-07-03 01:02:00 +0100

    create playlistsRouter and getHistory procedure

---
+++ src/modules/playlists/server/procedures.ts
@@ -0,0 +1,79 @@
+import { db } from "@/db";
+import { users, videos, videoViews } from "@/db/schema";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { and, desc, eq, getTableColumns, lt, or } from "drizzle-orm";
+import { z } from "zod";
+
+export const playlistsRouter = createTRPCRouter({
+  getHistory: protectedProcedure
+    .input(
+      z.object({
+        cursor: z
+          .object({
+            id: z.string().uuid(),
+            viewedAt: z.date(),
+          })
+          .nullish(),
+        limit: z.number().min(1).max(100),
+      })
+    )
+    .query(async ({ input, ctx }) => {
+      const { id: userId } = ctx.user;
+      const { cursor, limit } = input;
+
+      const viewerVideoViews = db.$with("viewer_video_views").as(
+        db
+          .select({
+            videoId: videoViews.videoId,
+            viewedAt: videoViews.updatedAt,
+          })
+          .from(videoViews)
+          .where(eq(videoViews.userId, userId))
+      );
+
+      const data = await db
+        .with(viewerVideoViews)
+        .select({
+          ...getTableColumns(videos),
+          user: users,
+          viewCount: db.$count(videoViews, eq(videoViews.videoId, videos.id)),
+          viewedAt: viewerVideoViews.viewedAt,
+        })
+        .from(videos)
+        .innerJoin(users, eq(videos.userId, users.id))
+        .innerJoin(viewerVideoViews, eq(videos.id, viewerVideoViews.videoId))
+        .where(
+          and(
+            eq(videos.visibility, "public"),
+            cursor
+              ? or(
+                  lt(viewerVideoViews.viewedAt, cursor.viewedAt),
+                  and(
+                    eq(viewerVideoViews.viewedAt, cursor.viewedAt),
+                    lt(videos.id, cursor.id)
+                  )
+                )
+              : undefined
+          )
+        )
+        .orderBy(desc(viewerVideoViews.viewedAt), desc(videos.id))
+        // Add 1 to limit to check if more data to load
+        .limit(limit + 1);
+
+      const hasMore = data.length > limit;
+
+      // Remove last item if more data than requested
+      const items = hasMore ? data.slice(0, -1) : data;
+
+      // Set next cursor to last item if there is more data
+      const lastItem = items[items.length - 1];
+      const nextCursor = hasMore
+        ? {
+            id: lastItem.id,
+            updatedAt: lastItem.updatedAt,
+          }
+        : null;
+
+      return { items, nextCursor };
+    }),
+});
