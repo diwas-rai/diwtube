e03fae0a8b7b244915b0c8024a9fbbe96bbca18f
Diwas Rai <diwasrai38@yahoo.com>
2025-08-11 18:31:29 +0100

    add modal to create new playlist

---
+++ src/modules/playlists/ui/components/playlist-create-modal.tsx
@@ -0,0 +1,91 @@
+import { ResponsiveModal } from "@/components/responsive-dialog";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { trpc } from "@/trpc/client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
+interface PlaylistCreateModalProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
+export const PlaylistCreateModal = ({
+  open,
+  onOpenChange,
+}: PlaylistCreateModalProps) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const createPlaylist = trpc.playlists.create.useMutation({
+    onSuccess: () => {
+      toast.success("Playlist created");
+      form.reset();
+      onOpenChange(false);
+    },
+    onError: (error) => {
+      console.error(error);
+      toast.error("Something went wrong");
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    createPlaylist.mutate(values);
+    onOpenChange(false);
+  };
+
+  return (
+    <ResponsiveModal
+      title="Create a New Playlist"
+      open={open}
+      onOpenChange={onOpenChange}
+    >
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-4"
+        >
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    className="resize-none"
+                    placeholder="Playlist name..."
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex justify-end">
+            <Button type="submit" disabled={createPlaylist.isPending}>
+              Create
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </ResponsiveModal>
+  );
+};
