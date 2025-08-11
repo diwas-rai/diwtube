c2982fa075906a332dd851711afa5388a80ccf99
Diwas Rai <diwasrai38@yahoo.com>
2025-08-11 19:38:03 +0100

    set up prefetching for playlists in page component

--- src/app/(home)/playlists/page.tsx
+++ src/app/(home)/playlists/page.tsx
@@ -1,7 +1,17 @@
+import { DEFAULT_LIMIT } from "@/constants";
 import { PlaylistsView } from "@/modules/playlists/ui/views/playlists-view";
+import { HydrateClient, trpc } from "@/trpc/server";
 
 const Page = () => {
-  return <PlaylistsView />;
+  void trpc.playlists.getMany.prefetchInfinite({
+    limit: DEFAULT_LIMIT,
+  });
+
+  return (
+    <HydrateClient>
+      <PlaylistsView />
+    </HydrateClient>
+  );
 };
 
 export default Page;
