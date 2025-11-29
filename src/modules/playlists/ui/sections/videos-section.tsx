5c5af05411043eb1ddfd8c0ef9080d3e03146473
Diwas Rai <diwasrai38@yahoo.com>
2025-11-29 16:42:51 +0000

    add functionality to remove videos from playlist section

--- src/modules/playlists/ui/sections/videos-section.tsx
+++ src/modules/playlists/ui/sections/videos-section.tsx
@@ -10,6 +10,7 @@ import { VideoRowCard } from "@/modules/videos/ui/components/video-row-card";
 import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import { toast } from "sonner";
 
 interface VideosSectionProps {
   playlistId: string;
@@ -19,7 +20,7 @@ export const VideosSection = ({ playlistId }: VideosSectionProps) => {
   return (
     <Suspense fallback={<VideosSectionSkeleton />}>
       <ErrorBoundary fallback={<p>Error...</p>}>
-        <VideosSectionSuspense playlistId={playlistId}/>
+        <VideosSectionSuspense playlistId={playlistId} />
       </ErrorBoundary>
     </Suspense>
   );
@@ -35,7 +36,8 @@ const VideosSectionSkeleton = () => {
   );
 };
 
-const VideosSectionSuspense = ({playlistId}: VideosSectionProps) => {
+const VideosSectionSuspense = ({ playlistId }: VideosSectionProps) => {
+  const utils = trpc.useUtils();
   const [videos, { hasNextPage, isFetchingNextPage, fetchNextPage }] =
     trpc.playlists.getVideos.useSuspenseInfiniteQuery(
       { playlistId, limit: DEFAULT_LIMIT },
@@ -46,16 +48,44 @@ const VideosSectionSuspense = ({playlistId}: VideosSectionProps) => {
 
   const watchedVideos = videos.pages.flatMap((page) => page.items);
 
+  const removeVideo = trpc.playlists.removeVideo.useMutation({
+    onSuccess: (data) => {
+      toast.success("Video successfully removed from playlist");
+      utils.playlists.getMany.invalidate();
+      utils.playlists.getManyWithVideoStatus.invalidate({
+        videoId: data.videoId,
+      });
+      utils.playlists.getOne.invalidate({ playlistId: data.playlistId });
+      utils.playlists.getVideos.invalidate({ playlistId: data.playlistId });
+    },
+    onError: () => {
+      toast.error("Something went wrong");
+    },
+  });
+
   return (
     <div>
       <div className="flex flex-col gap-4 gap-y-10 md:hidden">
         {watchedVideos.map((video) => (
-          <VideoGridCard key={video.id} data={video} />
+          <VideoGridCard
+            key={video.id}
+            data={video}
+            onRemove={() =>
+              removeVideo.mutate({ playlistId, videoId: video.id })
+            }
+          />
         ))}
       </div>
       <div className="hidden flex-col gap-4 md:flex">
         {watchedVideos.map((video) => (
-          <VideoRowCard key={video.id} data={video} />
+          <VideoRowCard
+            key={video.id}
+            data={video}
+            size="compact"
+            onRemove={() =>
+              removeVideo.mutate({ playlistId, videoId: video.id })
+            }
+          />
         ))}
       </div>
       <InfiniteScroll
